cca6b04bb8835af28072b55860268627ba7d0872
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2023-09-10 23:14:13 +0200

    dados 07 conserta as div

--- PacMat-Contabilidade/src/scripts/main.js
+++ PacMat-Contabilidade/src/scripts/main.js
@@ -11,7 +11,17 @@ resultValorTotal = document.querySelector('#valor_total')// onde vai o resultado
 const diasSeguro = document.querySelector('#interacao_quantidades__dias__seguro');//capturando input de dias do seguro
 const precoSeguro = document.querySelector('#interacao_preco__seguro');//capiturando o preco do seguro
 const resultadoValorSeguro = document.querySelector('#valor_total__seguro')
-
+const ACCLOC = document.querySelector('#ACCLOC')//Capiturando a div
+const caution = document.querySelector('#interacao_preco__caution')//capiturando o caution
+const transporte = document.querySelector('#LOCZ');//capturando a div de transporte
+const transporteIdaVolta = document.querySelector('#interacao_quantidades__dias__transporte');// capturando valor do input ida e volta
+const inputValorTransport = document.querySelector('#interacao_preco__transporte');//capiturando o input do preco do transporte
+const resultadoTransporte = document.querySelector('#valor_total__transporte')//Onde o resultado do transporte vai
+const inputLinpeza = document.querySelector('#interacao_preco__limpesa');//inpute do valor da limpeza.
+const PM0133 = document.querySelector('#PM0133');//div do diesel
+const inputLitrosDisel = document.querySelector('#ĺitrosPm0133');//Litros de disel usando
+const inputPrecoDiesel = document.querySelector('#precoPm0133');//valor do diesel
+const paragrofoResultadoDisel = document.querySelector('#resultadoPm0133');//onde vai o resultado 
 
 
 botaoEnviar.addEventListener('click', function(ev) {
@@ -28,13 +38,14 @@ botaoEnviar.addEventListener('click', function(ev) {
     //ejetando o n° de serie
     const valorSerie = serie.value;
     spanSerie.textContent = valorSerie;
-    calcularDias()
-    calculaSeguro()
-
+    calcularDias();
+    calculaSeguro();
+    cautionValor();
+    transporteLoC();
+    limpeza();
+    diesel()
 
 });
-
-
 function calcularDias() {
     const precoMaquinaNum = parseFloat(precoPorDia.value);
     const diasAlugadosNum = parseFloat(diasAlugados.value);
@@ -42,12 +53,59 @@ function calcularDias() {
     resultValorTotal.textContent = somaDiaPreco.toFixed(2);
     return somaDiaPreco;
 }
-
 function calculaSeguro() {
     const diasSeguroNum = parseFloat(diasSeguro.value);
     precoSeguroNum = parseFloat(precoSeguro.value);
     const somaDiaPrecoSeguro = precoSeguroNum * diasSeguroNum;
     const procentagenSeguro = (somaDiaPrecoSeguro * 10) / 100;
     resultadoValorSeguro.textContent = procentagenSeguro.toFixed(2)
-    return resultadoValorSeguro;
+    return procentagenSeguro;
 }
+function cautionValor(){
+    const valorCaution = parseFloat(caution.value);
+    if(valorCaution == 0){
+        ACCLOC.style.display = 'none';
+    }else{
+        const cautionResultado = document.querySelector('#valor_total__caution');
+        cautionResultado.textContent = `-${valorCaution},00`;
+    }
+
+    return valorCaution;
+}
+function transporteLoC(){
+    const transporte = parseFloat(transporteIdaVolta.value);
+    if(transporte == 0){
+        LOCZ.style.display = 'none';
+    }else{       
+        const transporteIdaVoltaNum = parseFloat(transporteIdaVolta.value)
+        const precoTransporte = parseFloat(inputValorTransport.value)
+        const totalDoTransporte = precoTransporte * transporteIdaVoltaNum
+        resultadoTransporte.textContent = `${totalDoTransporte},00`
+        return totalDoTransporte
+    }
+   
+}
+function limpeza(){
+    const valorLimpeza = parseFloat(inputLinpeza.value);
+    if(valorLimpeza == 0){
+        PM014.style.display = 'none';
+    }else{
+        const valorLimpezaRsultado = document.querySelector('#valor_total__limpesa');
+        valorLimpezaRsultado.textContent = `${valorLimpeza},00`;
+    }
+
+    return valorLimpeza;
+}
+function diesel(){
+    const litrosDiesel = parseFloat(inputLitrosDisel.value);
+    
+    if(litrosDiesel == 0){
+        PM0133.style.display = 'none';
+    }else{
+        const precoDoDiesel = parseFloat(inputPrecoDiesel.value);
+        const totaPagarDiesel = precoDoDiesel * litrosDiesel;
+        paragrofoResultadoDisel.textContent = `${totaPagarDiesel}00`
+        return totaPagarDiesel;
+}
+}
+    
