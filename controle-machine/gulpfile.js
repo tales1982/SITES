f4d28cd934072a52f97e8e303c398b3eb797d053
Tales Lima <142450583+TalesPortfolio@users.noreply.github.com>
2023-10-29 23:31:41 +0100

    INICIANDO PROJETO

--- controle-machine/gulpfile.js
+++ controle-machine/gulpfile.js
@@ -1,33 +1,38 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
-const imagemin = require ('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
 
-function compresionJavacript(){
-    return gulp.src('./src/script/*.js')
+function copyBootstrap() {
+  return gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(gulp.dest('./build/style')); // Altere o destino conforme necessário
+}
+
+function compresionJavascript() {
+  return gulp.src('./src/script/*.js')
     .pipe(uglify())
-.pipe(gulp.dest('./build/script'))
+    .pipe(gulp.dest('./build/script'));
 }
 
-function compresionImage(){
-    return gulp.src('./src/images/*')
+function compresionImage() {
+  return gulp.src('./src/images/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('./build/images'))
+    .pipe(gulp.dest('./build/images'));
 }
 
-function compilaSass(){
-    return gulp.src('./src/style/*.scss')
+function compilaSass() {
+  return gulp.src('./src/style/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./build/style'))
+    .pipe(gulp.dest('./build/style'));
 }
 
-
-exports.default = function(){
-    gulp.watch('./src/style/*.scss',{ignoreInitial:false}, gulp.series(compilaSass));
-    gulp.watch('./src/script/*.js',{ignoreInitial:false}, gulp.series(compresionJavacript));
-    gulp.watch('./src/images/*',{ignoreInitial:false}, gulp.series(compresionImage));
+exports.default = function () {
+  gulp.watch('./src/style/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+  gulp.watch('./src/script/*.js', { ignoreInitial: false }, gulp.series(compresionJavascript));
+  gulp.watch('./src/images/*', { ignoreInitial: false }, gulp.series(compresionImage));
 }
 
 exports.sass = compilaSass;
-exports.javaScript = compresionJavacript;
+exports.javascript = compresionJavascript;
 exports.images = compresionImage;
+exports.bootstrap = copyBootstrap;
