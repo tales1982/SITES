d3db58bfca72b3d5471697e53a9a5ab10a640911
TalesPortfolio <psycho_bylly@hotmail.lu>
2023-11-19 21:10:51 +0100

    Delete sites

--- controle-machine/build/script/setPdf.js
+++
@@ -1,1 +0,0 @@
-const btnEnviar=document.querySelector("#gerar-pdf");btnEnviar.addEventListener("click",()=>{document.querySelector("#conteudoHTML");html2pdf().set({magin:[10,10,10,10],filename:"arquivo.pdf",html2canvas:{scale:2},jsPDF:{unit:"mm",format:"a4",orientation:"portrait"}}).from(conteudoHTML).save()});
